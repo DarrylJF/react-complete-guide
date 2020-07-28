333cc9aa906045fd3f2e3490afa87a0d93e64ae0
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-07-28 20:24:40 +0100

    47. Adding two way binding

--- src/App.js
+++ src/App.js
@@ -1,44 +1,97 @@
-import React, { useState} from 'react';
+//import React, { useState, Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [ personState, setPersonsState ] = useState({
+class App extends Component {
+
+  state = {
     persons: [
-      { name: "J", age: 33 },
-      { name: "B", age: 21 },
-      { name: "G", age: 22}
-  ],
-  });
+      { name: "name 1", age: 28 },
+      { name: "name 2", age: 18 },
+      { name: "name 3", age: 44 }
+    ],
+    otherState: 'some other value'
+  }
 
-  const [ otherState, setOtherState ] = useState('some other state')
-
-  console.log(personState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState( {
+  switchNameHandler = (newName) => {
+    this.setState( {
       persons: [
-        { name: "T", age: 37 },
-        { name: "B", age: 21 },
-        { name: "G", age: 23}
+        { name: newName, age: 32 },
+        { name: "name 2", age: 18 },
+        { name: "name 8", age: 67 }
       ]
-    });
-  };
+    })
+  }
 
+  nameChangeHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: event.target.value, age: 23 },
+        { name: "name 2", age: 18 },
+        { name: "name 3", age: 44 }
+      ]
+    })
+  }
 
-  return (
+  render() {
+    return (
       <div className="App">
-        <h1>this is a test</h1>
-          <p>this is a paragraph</p>
-          <button onClick={switchNameHandler}>Switch Name</button>
-          <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-          <Person name={personState.persons[1].name} age={personState.persons[1].age}/>
-          <Person name={personState.persons[2].name} age={personState.persons[2].age}></Person>
+         <h1>this is a test</h1>
+           <p>this is a paragraph</p>
+          <button onClick={() => this.switchNameHandler('max!!')}>Switch Name</button>
+           <Person
+          name={this.state.persons[0].name} age={this.state.persons[0].age} changed={this.nameChangeHandler} />
+          <Person 
+            name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'max!')} />
+          <Person 
+            name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
-   // JSX, compiles to
-   // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'does this work now'));
-  
+  }
 }
 
-export default app;
+export default App;
+// const app = props => {
+//   const [ personState, setPersonsState ] = useState({
+//     persons: [
+//       { name: "J", age: 33 },
+//       { name: "B", age: 21 },
+//       { name: "G", age: 22}
+//   ],
+//   });
+
+//   const [ otherState, setOtherState ] = useState('some other state')
+
+//   console.log(personState, otherState);
+
+//   const switchNameHandler = () => {
+//     setPersonsState( {
+//       persons: [
+//         { name: "T", age: 37 },
+//         { name: "B", age: 21 },
+//         { name: "G", age: 23}
+//       ]
+//     });
+//   };
+
+
+//   return (
+//       <div className="App">
+//         <h1>this is a test</h1>
+//           <p>this is a paragraph</p>
+//           <button onClick={switchNameHandler}>Switch Name</button>
+//           <Person
+//             name={personState.persons[0].name} age={personState.persons[0].age}/>
+//           <Person 
+//             name={personState.persons[1].name} age={personState.persons[1].age} click={switchNameHandler}/>
+//           <Person 
+//             name={personState.persons[2].name} age={personState.persons[2].age}/>
+//       </div>
+//     );
+//    // JSX, compiles to
+//    // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'does this work now'));
+  
+// }
+
+
