b755b983b9dafe23dff1bf0649237e62bbc300a9
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-08-13 11:43:11 +0100

    outputting data conditionally

--- src/App.js
+++ src/App.js
@@ -1,145 +1,74 @@
-//import React, { useState, Component} from 'react';
+// import React, { useState, Component} from 'react';
 import React, { Component } from 'react';
 import './App.css';
-
+import './'
 import Person from './Person/Person';
 
 class App extends Component {
-
   state = {
     persons: [
-      { id: 1, name: "name 1", age: 28 },
-      { id: 2, name: "name 2", age: 18 },
-      { id: 3, name: "name 3", age: 44 }
+      { name: 'Johnson', age: 28 },
+      { name: 'Abhubaka', age: 33},
+      { name: 'Name 3', age: 44}
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState( {persons: persons} );
+  switchNameHandler = (newName) => {
+    // console.log('clicked');
+    // DONT DO THIS: this.state.persons[0].name = 'New Name'; (Dont update the data directly)
+    this.setState( {
+      persons: [
+        { name: newName, age: 66 },
+        { name: 'Name 2', age: 30},
+        { name: 'Name 3', age: 44}
+      ]
+    } )
   }
 
-  togglePersonHandler = () => {
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { name: 'Name 1', age: 66 },
+        { name: event.target.value, age: 30},
+        { name: 'Name 3', age: 44}
+      ]
+    } )
+  }
+
+  togglePersonsHandler = ( ) => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow}); // set showPersons == to what does show is not. doesShow is equal to the showPersons state which is false so it will set it to true
-  }
-
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
-    // always update state in an immutable fashion. create a copy, makes changes to the copy, and then update state using setState()
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    this.setState({showPersons: !doesShow})
   }
 
   render() {
-    const style = {
-      backgroundColor: 'orange',
-      font: 'inherit',
-      padding: '8px',
-      color: 'white',
-      cursor: 'pointer'
-    };
-
-    let persons = null; // persons is null by default (does not show)
-
-    if ( this.state.showPersons ) { // if showPersons is true (show)
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age} 
-              key={person.id} // default property react expects to find on an element which is rendered via a list (map)
-              changed={(event) => this.nameChangeHandler(event, person.id)}
-              /> // end of Person Component
-          })}
-        </div>
-      );
-    }
-
     return (
       <div className="App">
-         <h1>this is a test</h1>
-           <p>this is a paragraph</p>
-          <button 
-            style={style}
-            onClick={this.togglePersonHandler}>Show/Hide
-          </button>
-          {persons}          
+          <h1>Title</h1>
+          <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
+
+          { this.state.showPersons ? 
+              <div>
+                <Person
+                  name={this.state.persons[0].name}
+                  age={this.state.persons[0].age}>
+                    MY HOBBIES ARE
+                </Person>
+                <Person
+                  name={this.state.persons[1].name}
+                  age={this.state.persons[1].age}
+                  click={() => this.switchNameHandler('ANONYMOUS ARROW FUNCTION')} // ANONYMOUS FUNCTION (dont recommend using this method if dont have to)
+                  changed={this.nameChangedHandler}/>
+                <Person 
+                  name={this.state.persons[2].name}
+                  age={this.state.persons[2].age}/>
+              </div> : null
+          } 
       </div>
     );
   }
 }
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-// const app = props => {
-//   const [ personState, setPersonsState ] = useState({
-//     persons: [
-//       { name: "J", age: 33 },
-//       { name: "B", age: 21 },
-//       { name: "G", age: 22}
-//   ],
-//   });
-
-//   const [ otherState, setOtherState ] = useState('some other state')
-
-//   console.log(personState, otherState);
-
-//   const switchNameHandler = () => {
-//     setPersonsState( {
-//       persons: [
-//         { name: "T", age: 37 },
-//         { name: "B", age: 21 },
-//         { name: "G", age: 23}
-//       ]
-//     });
-//   };
-
-
-//   return (
-//       <div className="App">
-//         <h1>this is a test</h1>
-//           <p>this is a paragraph</p>
-//           <button onClick={switchNameHandler}>Switch Name</button>
-//           <Person
-//             name={personState.persons[0].name} age={personState.persons[0].age}/>
-//           <Person 
-//             name={personState.persons[1].name} age={personState.persons[1].age} click={switchNameHandler}/>
-//           <Person 
-//             name={personState.persons[2].name} age={personState.persons[2].age}/>
-//       </div>
-//     );
-//    // JSX, compiles to
-//    // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'does this work now'));
-  
-// }
 
 
