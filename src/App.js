2aa59afd1cf24e292bdfb29ab9ae93db777c1e18
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-08-17 13:17:58 +0100

    73 working with css modules

--- src/App.js
+++ src/App.js
@@ -1,74 +1,124 @@
 // import React, { useState, Component} from 'react';
 import React, { Component } from 'react';
 import './App.css';
-import './'
+import styled from 'styled-components';
+// import Radium, { StyleRoot } from 'radium'; 
 import Person from './Person/Person';
 
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+    &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+`;
+
 class App extends Component {
-  state = {
+  state = { // this is the data source (in bigger applications this data will be fetched from a server)
     persons: [
-      { name: 'Johnson', age: 28 },
-      { name: 'Abhubaka', age: 33},
-      { name: 'Name 3', age: 44}
+      { id: 1, name: 'Johnson', age: 28 },
+      { id: 2, name: 'Abhubaka', age: 33},
+      { id: 3, name: 'Name 3', age: 44}
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('clicked');
-    // DONT DO THIS: this.state.persons[0].name = 'New Name'; (Dont update the data directly)
-    this.setState( {
-      persons: [
-        { name: newName, age: 66 },
-        { name: 'Name 2', age: 30},
-        { name: 'Name 3', age: 44}
-      ]
-    } )
-  }
 
-  nameChangedHandler = ( event ) => {
-    this.setState( {
-      persons: [
-        { name: 'Name 1', age: 66 },
-        { name: event.target.value, age: 30},
-        { name: 'Name 3', age: 44}
-      ]
-    } )
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(prson => {
+      return prson.id === id;
+    });
+    
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons]; // create a reference to persons state array
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} )
   }
 
   togglePersonsHandler = ( ) => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
+    this.setState({ showPersons: !doesShow })
+  }
+
+  deletePersonhandler = ( personIndex ) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]; // spread operator
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   render() {
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map(( person, index ) => {
+            return <Person 
+                    changed={(event) => this.nameChangedHandler(event, person.id)}
+                    click={() => this.deletePersonhandler(index)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}/>
+          })}
+          </div> 
+      );
+
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // };
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
-          <h1>Title</h1>
-          <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
-
-          { this.state.showPersons ? 
-              <div>
-                <Person
-                  name={this.state.persons[0].name}
-                  age={this.state.persons[0].age}>
-                    MY HOBBIES ARE
-                </Person>
-                <Person
-                  name={this.state.persons[1].name}
-                  age={this.state.persons[1].age}
-                  click={() => this.switchNameHandler('ANONYMOUS ARROW FUNCTION')} // ANONYMOUS FUNCTION (dont recommend using this method if dont have to)
-                  changed={this.nameChangedHandler}/>
-                <Person 
-                  name={this.state.persons[2].name}
-                  age={this.state.persons[2].age}/>
-              </div> : null
-          } 
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working</p>
+          <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+            Toggle Person
+          </StyledButton>
+          {persons}
       </div>
     );
   }
 }
+
 export default App;
+// export default Radium(App);
 
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import './Person.css';
+// import './Person.css';
+// import Radium from 'radium';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+width: 60%;
+margin: 1rem auto;
+padding: 1.5rem;
+border: 1px solid rgb(50, 138, 221);
+box-shadow: 0 3px 5px #ccc;
+text-align: center;
+
+@media (min-width: 500px) {
+    width: 450px;
+}
+`;
 
 const person = ( props ) => {
     return (
-        <div className="Person">
-            <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
+    <StyledDiv>
+        <p onClick={props.click}>
+            I am {props.name} and I am {props.age} years old
+        </p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
+    </StyledDiv>
     )
 };
 
