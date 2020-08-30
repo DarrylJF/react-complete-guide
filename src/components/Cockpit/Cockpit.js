4e64a6ca09caa98595baa2a6d92b8ed6e0943d39
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-08-31 00:40:42 +0100

    114.contextType & useContext()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.css';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = props => {
   const toggleButtonRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
 
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
@@ -45,9 +48,7 @@ const cockpit = props => {
         ref={toggleButtonRef}>
         Toggle Persons
       </button>
-      <AuthContext.Consumer>
-        {(context) => <button onClick={context.login}>Login</button>}
-      </AuthContext.Consumer>
+        <button onClick={authContext.login}>Login</button>
     </div>
   );
 };
